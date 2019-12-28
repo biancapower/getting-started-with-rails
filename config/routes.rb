716ed19bfec973b45375ac33046b1792d38c8823
Bianca Power <30207144+biancapower@users.noreply.github.com>
2019-12-28 16:30:49 +1100

    add articles resource to 'config/routes.rb'

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   # this was created earlier when we ran the controller generator (rails generate controller Welcome index)
   get 'welcome/index'
 
+  # 'resources' declares a standard REST resource
+  # resource is the term used for a collection of similar objects, such as articles, people, or animals
+  # run the command `rails routes` in terminal, and you'll see that it has defined routes for all the standard RESTful actions
+  resources :articles
+
   # tells Rails to map requests to the root of the application (http://localhost:3000/) to the welcome controller's index action
   root 'welcome#index'
 end

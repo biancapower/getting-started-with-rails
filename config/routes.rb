60884e493cdc380114a9d6ac417998113e0b4fa3
Bianca Power <30207144+biancapower@users.noreply.github.com>
2019-12-28 16:14:35 +1100

    add root route to `config/routes.rb`

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  # tells Rails to map requests to http://localhost:3000/welcome/index to the welcome controller's index action
+  # this was created earlier when we ran the controller generator (rails generate controller Welcome index)
   get 'welcome/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # tells Rails to map requests to the root of the application (http://localhost:3000/) to the welcome controller's index action
+  root 'welcome#index'
 end
